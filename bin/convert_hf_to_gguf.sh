602c9cb58e867e5b4216681084ce4ac585dae566
Nick Mazurkin <mazurkin@gmail.com>
2025-12-29 18:56:29 -0500

    update

---
+++ bin/convert_hf_to_gguf.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o monitor
+set -o noglob
+
+# calculate the current directory
+SCRIPT_DIR=$(dirname -- "$(readlink -f -- "${BASH_SOURCE[0]}")")
+declare -r SCRIPT_DIR
+
+# calculate the package directory
+PACKAGE_DIR=$(dirname -- "${SCRIPT_DIR}")
+declare -r PACKAGE_DIR
+
+# python environment
+export PYTHONDONTWRITEBYTECODE=1
+export PYTHONUNBUFFERED=1
+
+# run script
+conda run --no-capture-output --live-stream --name llamacpp --cwd "${PACKAGE_DIR}/llamacpp" \
+    python3 convert_hf_to_gguf.py \
+        "$@"
